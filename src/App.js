b56c6d91cb0bf48429606d2dff3d4b8fe537e276
cricrich2010 <fr56243@macRQQXPJLGFP.local>
2022-11-25 17:37:34 +0100

    update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [username, setUsername] = useState("");
   const [message, setMessage] = useState(false);
-  
+
   // const BACKEND_URL = "YOUR_BACKEND_URL"
-  const BACKEND_URL = "http://localhost:8080"
+  const BACKEND_URL = "http://ec2-user@ec2-15-188-14-80.eu-west-3.compute.amazonaws.com:8080"
   const onChangeHandler = (event) => {
     console.log(event.target.files[0])
     setSelectedFile(event.target.files[0]);
@@ -24,19 +24,19 @@ function App() {
     const data = new FormData();
     data.append('file', selectedFile);
     data.append('username', username);
-    console.log("data",data)
-    axios.post(`${BACKEND_URL}/api/upload`, data, 
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      }
-    })
+    console.log("data", data)
+    axios.post(`${BACKEND_URL}/api/upload`, data,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        }
+      })
       .then((res) => {
         console.log(res);
-        setMessage({status:"success",message:"file uploaded successfully"});
+        setMessage({ status: "success", message: "file uploaded successfully" });
       })
       .catch((err) => {
-        setMessage({status:"fail",message:"there was an error in the upload: "+JSON.stringify(err)});
+        setMessage({ status: "fail", message: "there was an error in the upload: " + JSON.stringify(err) });
         console.log(err);
       });
   };
@@ -45,16 +45,16 @@ function App() {
   return (
     <div className="App">
       <form className="main-form">
-        <input type="text" placeholder="your username" name="username" value={username} onChange={onUsernameChangeHandler}/>
+        <input type="text" placeholder="your username" name="username" value={username} onChange={onUsernameChangeHandler} />
         <div className="file-container">
-        <input type="file" name="file" onChange={onChangeHandler}/>
-        <p>{selectedFile ? selectedFile.name :"Drag your files here or click in this area."}</p>
+          <input type="file" name="file" onChange={onChangeHandler} />
+          <p>{selectedFile ? selectedFile.name : "Drag your files here or click in this area."}</p>
         </div>
         <button type="submit" onClick={onClickHandler}>Upload</button>
       </form>
-      
+
       <br />
-      {(message && <p className={"status "+message.status}>{message.message}</p>)}
+      {(message && <p className={"status " + message.status}>{message.message}</p>)}
     </div>
   );
 }
